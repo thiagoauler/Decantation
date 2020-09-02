5abb5e6b27a41c91f98f37321cb9312642e63e2f
Thiago Auler dos Santos <thiago@imac.local>
2020-09-02 10:26:05 -0300

    Initial Commit

---
+++ Decantation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Decantation
+//
+//  Created by Thiago Auler dos Santos on 02/09/2020.
+//  Copyright © 2020 Thiago Auler dos Santos. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
