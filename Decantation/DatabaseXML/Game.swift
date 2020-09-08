fbfb967ec5adff45e420fedd2f926aeeb1c53d6a
Thiago Auler dos Santos <thiago@imac.local>
2020-09-08 18:00:59 -0300

    Prioritizing roms by region

--- Decantation/DatabaseXML/Game.swift
+++ Decantation/DatabaseXML/Game.swift
@@ -14,4 +14,74 @@ class Game
     var cloneof: String?
     var releases = [Release]()
     var rom = Rom()
+    
+    var key: String
+    {
+        get
+        {
+            // the hash key will be the name of the game...
+            var key = self.name
+            if let cloneof = self.cloneof
+            {
+                //... or the parent's
+                key = cloneof
+            }
+            return key
+        }
+    }
+    
+    func getRegionPriority(from selectedRegions: [String]) -> Int
+    {
+        for currentRegion in selectedRegions
+        {
+            if self.isFrom(region: currentRegion)
+            {
+                return selectedRegions.firstIndex(of: currentRegion)!
+            }
+        }
+        return Int.max
+    }
+    
+    func isFrom(region: String) -> Bool
+    {
+        return self.name ~= "\\(.*\(region).*\\)"
+    }
+    
+    func isUnreleased() -> Bool
+    {
+        return self.isFrom(region: "Alt") ||
+               self.isFrom(region: "Beta") ||
+               self.isFrom(region: "Demo") ||
+               self.isFrom(region: "Pirate") ||
+               self.isFrom(region: "Program") ||
+               self.isFrom(region: "Proto") ||
+               self.isFrom(region: "Sample") ||
+               self.isFrom(region: "Test Program") ||
+               self.isFrom(region: "Unl")
+    }
+    
+    func isDigital() -> Bool
+    {
+        return self.isFrom(region: "Archives") ||
+               self.isFrom(region: "Collection") ||
+               self.isFrom(region: "Edition") ||
+               self.isFrom(region: "Pack") ||
+               self.isFrom(region: "Sega Channel") ||
+               self.isFrom(region: "SegaNet") ||
+               self.isFrom(region: "Version") ||
+               self.isFrom(region: "Virtual Console")
+    }
+    
+    func isCollection() -> Bool
+    {
+        return self.name ~= "\\d* in 1" ||
+               self.name ~= "\\d*-in-1" ||
+               self.name ~= "\\d*-Pak" ||
+               self.name ~= ".* \\+ .*" && !(self.name ~= "Sonic")
+    }
+    
+    func isBios() -> Bool
+    {
+        return self.name ~= "\\[BIOS\\]"
+    }
 }
--- Decantation/main.swift
+++ Decantation/main.swift
@@ -8,6 +8,80 @@
 
 import Foundation
 
+func isEligible(database: [String: Game], game: Game, regions: [String]) -> Bool
+{
+    if game.isUnreleased()
+    {
+        game.name = "[UNOFFICIAL] " + game.name
+        return false
+    }
+    
+    if game.isDigital()
+    {
+        game.name = "[DIGITAL] " + game.name
+        return false
+    }
+    
+    if game.isCollection()
+    {
+        game.name = "[PACK] " + game.name
+        return false
+    }
+    
+    if game.isBios()
+    {
+        return false
+    }
+    
+    let currentGamePriority = game.getRegionPriority(from: regions)
+    
+    if let existingGame = database[game.key]
+    {
+        //in case there is some game whith the provided key,
+        //we need to match the region...
+        let existingGamePriority = existingGame.getRegionPriority(from: regions)
+        
+        if currentGamePriority < existingGamePriority
+        {
+            // the current game region has higher priority over existing game region
+            return true
+        }
+        
+        if currentGamePriority == existingGamePriority
+        {
+            // both game has equal priority...
+            //... first, we compare if the existing game is the actual parent of the release
+            if game.cloneof == existingGame.name
+            {
+                return false
+            }
+            
+            //... then, we compare the other way around
+            if existingGame.cloneof == game.name
+            {
+                return true
+            }
+            
+            //... finally, let's compare the names
+            if game.name > existingGame.name
+            {
+                return true
+            }
+        }
+    }
+    else
+    {
+        //... otherwise, we insert it!
+        if currentGamePriority < regions.count
+        {
+            return true
+        }
+    }
+    
+    return false
+}
+
+
 if CommandLine.argc == 1
 {
     print("Database required.")
@@ -24,52 +98,54 @@ let database = Parser.perform(datFilePath: datFilePath)
 
 var ignoredGames = [Game]()
 var validGames = [String: Game]()
-for game in database.values
+
+for currentGame in database.values
 {
-    if game.name.contains("(Beta)") ||
-       game.name.contains("(Proto)") ||
-       game.name.contains("(Proto 1)") ||
-       game.name.contains("(Proto 2)") ||
-       game.name.contains("(Unl)")
+    if isEligible(database: validGames, game: currentGame, regions: ["Brazil", "World", "USA", "Europe"])
     {
-        ignoredGames.append(game)
+        if let previousValidGame = validGames[currentGame.key]
+        {
+            previousValidGame.cloneof = currentGame.name
+            ignoredGames.append(previousValidGame)
+        }
+        validGames[currentGame.key] = currentGame
     }
     else
     {
-        var key = game.name
-        if let cloneof = game.cloneof
+        if let currentValidGame = validGames[currentGame.key]
         {
-            key = cloneof
-        }
-        
-        if validGames[key] == nil
-        {
-            validGames[key] = game
+            currentGame.cloneof = currentValidGame.name
         }
         else
         {
-            if game.name.contains("USA,") ||
-               game.name.contains("USA)")
-            {
-                validGames[key] = game
-                ignoredGames.append(validGames[key]!)
-            }
-            else
-            {
-                ignoredGames.append(game)
-            }
+            currentGame.cloneof = nil
         }
+        ignoredGames.append(currentGame)
     }
 }
 
 print("Ignored:")
-for game in ignoredGames
+for game in ignoredGames.sorted(by: { return $0.name < $1.name })
 {
-    print(game.rom.name)
+    print(game.name)
+    if let cloneof = game.cloneof
+    {
+        print(" -", cloneof)
+    }
 }
 
-print("\nDatabase:")
-for game in validGames.values
+/*print("\nDatabase:")
+for game in validGames.values.sorted(by: { return $0.name < $1.name })
 {
-    print(game.rom.name)
+    print(game.name)
+}*/
+
+extension String
+{
+    static func ~= (lhs: String, rhs: String) -> Bool
+    {
+        guard let regex = try? NSRegularExpression(pattern: rhs) else { return false }
+        let range = NSRange(location: 0, length: lhs.utf16.count)
+        return regex.firstMatch(in: lhs, options: [], range: range) != nil
+    }
 }
