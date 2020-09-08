5c12c8a755cd0f4c73a9092a03cee313460975dc
Thiago Auler dos Santos <thiago@imac.local>
2020-09-08 01:44:32 -0300

    Parsing valid games

--- Decantation/main.swift
+++ Decantation/main.swift
@@ -20,4 +20,56 @@ if !FileManager.default.fileExists(atPath: datFilePath)
     print("Database does not exist.")
 }
 
-print("Hello, World!")
+let database = Parser.perform(datFilePath: datFilePath)
+
+var ignoredGames = [Game]()
+var validGames = [String: Game]()
+for game in database.values
+{
+    if game.name.contains("(Beta)") ||
+       game.name.contains("(Proto)") ||
+       game.name.contains("(Proto 1)") ||
+       game.name.contains("(Proto 2)") ||
+       game.name.contains("(Unl)")
+    {
+        ignoredGames.append(game)
+    }
+    else
+    {
+        var key = game.name
+        if let cloneof = game.cloneof
+        {
+            key = cloneof
+        }
+        
+        if validGames[key] == nil
+        {
+            validGames[key] = game
+        }
+        else
+        {
+            if game.name.contains("USA,") ||
+               game.name.contains("USA)")
+            {
+                validGames[key] = game
+                ignoredGames.append(validGames[key]!)
+            }
+            else
+            {
+                ignoredGames.append(game)
+            }
+        }
+    }
+}
+
+print("Ignored:")
+for game in ignoredGames
+{
+    print(game.rom.name)
+}
+
+print("\nDatabase:")
+for game in validGames.values
+{
+    print(game.rom.name)
+}
